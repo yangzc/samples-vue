a8324d0e80f9d6bba1c75cbb1fe61e18e3b81bfa
yangzc <zhc_yang@163.com>
2023-09-04 23:36:27 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,14 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Welcome" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/test">Test</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
@@ -35,7 +36,7 @@ nav {
   width: 100%;
   font-size: 12px;
   text-align: center;
-  margin-top: 2rem;
+  margin-top: 1rem;
 }
 
 nav a.router-link-exact-active {
