4a3679a3828269bcfb216eb9632e5962264cae84
yangzc <zhc_yang@163.com>
2023-09-05 11:16:13 +0800

    dev

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
+    <button @click="click">Add</button>
     <ul style="height: 300px; overflow-y: auto" ref="ulRef">
-      <li v-for="item in list" :key="item">{{ item }}</li>
+      <li v-for="item in list" :key="item.id">{{ item.text }}</li>
+      <!-- {{
+        text
+      }} -->
     </ul>
   </div>
 </template>
@@ -9,48 +13,30 @@
 import { ref, onMounted } from 'vue'
 
 const ulRef = ref()
-const list = ref<string[]>([])
+const text = ref('')
+const list = ref<any>([])
 
 onMounted(() => {
   startDOMObserver(ulRef.value)
-
   console.log('onMounted...' + new Date().getTime())
-
-  // list.value.push('First Item')
-  // console.log("onMounted...1.." + new Date().getTime());
-
-  // setTimeout(() => {
-  //   console.log("setTimeout..." + new Date().getTime());
-  // }, 0);
-  // for (let i = 0; i < 50; i++) {
-  //   list.value.push('Item-' + i.toString())
-  // }
-  // console.log("onMounted...2.." + new Date().getTime());
-
-  setTimeout(() => {
-    list.value.push('First Item')
-    console.log('setTimeout2...' + new Date().getTime())
-
-    setTimeout(() => {
-      list.value.push('First Item11')
-      console.log('setTimeout3...' + new Date().getTime())
-    }, 0)
-
-    new Promise((resolve) => {
-      console.log('Promise...')
-      resolve(1)
-    }).then(() => {
-      console.log('Promise then...')
+  for (let i = 0; i < 50; i++) {
+    list.value.push({
+      text: 'Item-' + i
     })
-  }, 0)
+  }
 })
+
+const click = () => {
+  // list.value.push({
+  //   text: 'Item-'
+  // })
+  list.value.shift();
+}
 
 const startDOMObserver = (el: Node) => {
   const observer = new MutationObserver((mutationsList) => {
     for (const mutation of mutationsList) {
-      if (mutation.type === 'childList') {
-        console.log('DOM 已更新')
-      }
+      console.log(mutation.type)
     }
   })
 
